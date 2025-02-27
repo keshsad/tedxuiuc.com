7b0f7da777e83910408de64747f2d3ffe15a04b0
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2025-02-27 00:17:12 -0600

    add site details

---
+++ config/site.ts
@@ -0,0 +1,35 @@
+export const siteConfig = {
+  name: "tedxuiuc",
+  description: "Ideas worth spreading at the University of Illinois",
+  url: "tedxuiuc.com",
+  image: "https://tedxuiuc.com/og.png",
+  author: "keshsad.com",
+  links: {
+    instagram: "https://www.instagram.com/tedxuiuc",
+    github: "https://github.com/keshsad/tedxuiuc.com",
+  },
+  nav: [
+    {
+      title: "About",
+      href: "/about",
+    },
+    {
+      title: "Speakers",
+      href: "/speakers",
+    },
+    {
+      title: "Sponsors",
+      href: "/sponsors",
+    },
+    {
+      title: "Join",
+      href: "/join",
+    },
+  ],
+  eventDetails: {
+    date: "April 13, 2025",
+    time: "18:00 to 21:00",
+    location: "Spurlock Musuem of World Cultures",
+    address: "600 S. Gregory St. Urbana, IL 61801",
+  }
+}
